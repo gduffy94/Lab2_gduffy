63fb03a896e6489bb6cc1c0cb7fba19516273f49
gduffy94 <gduffy@nd.edu>
2017-02-03 14:03:32 -0500

    Initial commit

---
+++ app/src/main/java/com/example/garrett/lab2_gduffy/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.garrett.lab2_gduffy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Garrett on 2/3/2017.
+ */
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread myThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
